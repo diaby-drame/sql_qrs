b844b53642bed4606d5ad290cdb880ef315ac38d
diaby-drame <kissimadrame72@gmail.com>
2024-01-04 17:50:19 +0100

    add the function to select the exercise and the function to check the result

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import logging
 import os
 import duckdb
+import pandas as pd
 import streamlit as st
 
 if "data" not in os.listdir():
@@ -14,6 +15,69 @@ if "exercises_sql_tables_duckdb" not in os.listdir("data"):
     exec(open("init_db.py").read())
 
 con = duckdb.connect(database="data/exercises_sql_tables_duckdb", read_only=False)
+
+
+def exercise_by_theme(user_theme, user_theme_exercise=None):
+    """
+    Choose the exercise according to the selected theme
+    :param user_theme: user selected theme
+    :param user_theme_exercise: user selected exercise by theme
+    :return: the exercise and exercise name
+    """
+
+    if user_theme_exercise:
+        exercise = (
+            con.execute(
+                f"SELECT * FROM memory_state WHERE theme = '{user_theme}' and exercise_name = '{user_theme_exercise}'"
+            )
+            .df()
+            .sort_values("last_reviewed")
+            .reset_index()
+        )
+        exercise_name = user_theme_exercise
+    else:
+        exercise = (
+            con.execute(f"SELECT * FROM memory_state WHERE theme = '{user_theme}'")
+            .df()
+            .sort_values("last_reviewed")
+            .reset_index()
+        )
+        exercise_name = exercise.loc[0, "exercise_name"]
+
+    return exercise, exercise_name
+
+
+def check_users_solution(user_query: str, solution: pd.DataFrame):
+    """
+    Checks that user SQL query is correct by:
+    1: checking the columns
+    2: checking the values
+    :param user_query: a string containing the query inserted by the user
+    :param solution: the correct data frame response
+    """
+
+    # user response
+    result = con.execute(user_query).df()
+    st.table(result)
+
+    try:
+        # if the user response is correct
+        result = result[solution.columns]
+        if result.compare(solution).shape == (0, 0):
+            st.write("Bravo !")
+            st.balloons()
+
+    except KeyError:
+        # if user response is not correct
+        st.write("Mauvaise réponse !")
+        st.info("Il manque des colonnes")
+        n_lines_difference = result.shape[0] - solution.shape[0]
+        if n_lines_difference != 0:
+            st.info(
+                f"Il y a {abs(n_lines_difference)} lignes de différence avec la solution"
+            )
+        exit()
+
 
 st.write(
     """
